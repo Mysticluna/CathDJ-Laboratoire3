34edec61e750c1de6f3a4e52cc98ce99237c9f73
Philippe Lafontaine <lafontaine.philippe@gmail.com>
2015-12-14 15:22:10 -0500

    Les bitmaps se transfer sans erreurs... Je sais juste pas pourquoi le fichier se sauve pas…

--- app/src/main/java/com/catherinedjobin/pam/cathdj_laboratoire3/MainActivity.java
+++ app/src/main/java/com/catherinedjobin/pam/cathdj_laboratoire3/MainActivity.java
@@ -3,10 +3,11 @@ package com.catherinedjobin.pam.cathdj_laboratoire3;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore.Images.Media;
-import android.support.v4.graphics.BitmapCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -17,13 +18,16 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 import android.widget.ViewSwitcher;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
-import java.util.Calendar;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity
         implements BluetoothConnectionManager.BluetoothConnectionHandler {
@@ -32,6 +36,8 @@ public class MainActivity extends AppCompatActivity
     private static final int REQUEST_SEND_IMAGE = 1337;
     private static final int MEGABYTE_BYTE_COUNT = 1024 * 1024;
     private static final int RGBA_PIXEL_BYTE_COUNT = 4;
+    private final File downloadDir =
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
     /**
      * Synchronize block object
      */
@@ -49,6 +55,9 @@ public class MainActivity extends AppCompatActivity
             this.viewSwitcher = (ViewSwitcher) this.findViewById(R.id.main_view_switcher);
             this.viewSwitcher.setInAnimation(this, android.R.anim.fade_in);
             this.viewSwitcher.setOutAnimation(this, android.R.anim.fade_out);
+        }
+        if (this.downloadDir.isDirectory() || this.downloadDir.mkdirs()) {
+            Log.d(TAG, "Download Directory: " + this.downloadDir.getAbsolutePath());
         }
     }
 
@@ -101,11 +110,10 @@ public class MainActivity extends AppCompatActivity
                             this.viewSwitcher.showNext();
                             Bitmap bitmap =
                                     Media.getBitmap(this.getContentResolver(), data.getData());
-                            int byteCount = BitmapCompat.getAllocationByteCount(bitmap);
-                            ByteBuffer buffer = ByteBuffer.allocate(byteCount);
-                            bitmap.copyPixelsToBuffer(buffer);
+                            ByteArrayOutputStream compressStream = new ByteArrayOutputStream();
+                            bitmap.compress(CompressFormat.JPEG, 100, compressStream);
+                            ByteBuffer buffer = ByteBuffer.wrap(compressStream.toByteArray());
                             Log.d(TAG, "Buffer: " + Arrays.toString(buffer.array()));
-                            buffer.rewind();
                             BluetoothConnectionManager.getInstance()
                                                       .connectToDevice(this.address, this, buffer);
                         } catch (FileNotFoundException e) {
@@ -179,16 +187,16 @@ public class MainActivity extends AppCompatActivity
                 }
                 if (buffer != null) {
                     Log.d(TAG, "Buffer: " + Arrays.toString(buffer.array()));
-                    // FIXME: BROKEN... :(
                     Bitmap bitmap = BitmapFactory.decodeByteArray(buffer.array(),
                                                                   buffer.arrayOffset(),
                                                                   buffer.position()
                                                                  );
                     if (bitmap != null) {
-                        String title = this.getString(R.string.app_name) + " "
-                                       + Calendar.getInstance().toString();
-                        // Sauvegarde de l'image
-                        Media.insertImage(this.getContentResolver(), bitmap, title, null);
+                        String name = UUID.randomUUID().toString() + ".jpeg";
+                        File file = new File(this.downloadDir, name);
+                        //FIXME: Ça sauve pas...
+                        FileOutputStream outputStream = new FileOutputStream(file);
+                        bitmap.compress(CompressFormat.JPEG, 100, outputStream);
                     } else {
                         this.runOnUiThread(new Runnable() {
                             @Override
